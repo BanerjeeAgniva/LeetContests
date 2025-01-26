c08eb8286ad080e10a487958c7094c26565a34c2
BanerjeeAgniva <90470431+BanerjeeAgniva@users.noreply.github.com>
2025-01-26 12:47:28 +0530

    1.cpp
    https://leetcode.com/problems/count-partitions-with-even-sum-difference/description/

---
+++ weekly434/1.cpp
@@ -0,0 +1,36 @@
+/*
+You are given an integer array nums of length n.
+
+A partition is defined as an index i where 0 <= i < n - 1, splitting the array into two non-empty subarrays such that:
+
+Left subarray contains indices [0, i].
+Right subarray contains indices [i + 1, n - 1].
+Return the number of partitions where the difference between the sum of the left and right subarrays is even.
+
+Input: nums = [10,10,3,7,6]
+
+Output: 4
+
+Explanation:
+
+The 4 partitions are:
+
+[10], [10, 3, 7, 6] with a sum difference of 10 - 26 = -16, which is even.
+[10, 10], [3, 7, 6] with a sum difference of 20 - 16 = 4, which is even.
+[10, 10, 3], [7, 6] with a sum difference of 23 - 13 = 10, which is even.
+[10, 10, 3, 7], [6] with a sum difference of 30 - 6 = 24, which is even.
+*/
+
+int countPartitions(vector<int>& nums) {
+        int sum=0;
+        for(int i=0;i<nums.size();i++) sum+=nums[i];
+        int count=0;
+        int left=nums[0];
+        for(int i=1;i<nums.size();i++)
+        {
+            int right=sum-left;
+            if((right-left) % 2 ==0) count++ ;
+            left+=nums[i];
+        }
+        return count;
+    }
